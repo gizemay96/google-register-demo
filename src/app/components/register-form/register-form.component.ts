929d452bf61da88404abcadb3d04965f73b180c3
Gizem Ay <gizem.ay1996@gmail.com>
2020-09-30 00:11:41 +0300

    Validation Error Messages & Styling

--- src/app/components/register-form/register-form.component.ts
+++ src/app/components/register-form/register-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { CustomvalidationService } from 'src/app/validations/customvalidation.service';
 
 @Component({
   selector: 'app-register-form',
@@ -7,36 +8,61 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./register-form.component.scss'],
 })
 export class RegisterFormComponent implements OnInit {
-  userForm = new FormGroup({
-    name: new FormControl('', [
-      Validators.required, Validators.minLength(3)
-    ]),
-    lastName: new FormControl('', [
-      Validators.required,
-      Validators.minLength(2),
-    ]),
-    email: new FormControl('', [
-      Validators.required, Validators.email
-    ]),
-    password: new FormControl('', [
-      Validators.required,
-      Validators.minLength(6),
-    ]),
-    confirmPass: new FormControl('', [
-      Validators.required,
-      Validators.minLength(6),
-    ]),
-  });
+  formError: boolean = false; // will be true if the userForm is invalid
 
-  constructor() {}
+  userForm = new FormGroup(
+    {
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      lastName: new FormControl('', [
+        Validators.required,
+        Validators.minLength(2),
+      ]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+      confirmPassword: new FormControl(''),
+    },
+    {
+      validators: this.customValidator.MatchPassword(
+        'password',
+        'confirmPassword'
+      ),
+    }
+  );
+
+  //    ----  GET ERRORS  ---- //
+  get nameErrors() {
+    return this.userForm.controls.name.errors;
+  }
+
+  get lastnameErrors() {
+    return this.userForm.controls.lastName.errors;
+  }
+
+  get emailErrors() {
+    return this.userForm.controls.email.errors;
+  }
+
+  get passwordErrors() {
+    return this.userForm.controls.password.errors;
+  }
+
+  get confirmPasswordErrors() {
+    return this.userForm.controls.confirmPassword.errors;
+  }
+
+  constructor(private customValidator: CustomvalidationService) {}
 
   ngOnInit(): void {}
 
   submitForm() {
     if (this.userForm.invalid) {
-      console.log('invalid');
+      console.log('invalid', this.userForm);
+      this.formError = true;
     } else {
-      console.log(this.userForm.value);
+      console.log(this.userForm);
     }
   }
 }
---
+++ src/app/validations/customvalidation.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CustomvalidationService {
+  MatchPassword(password: string, confirmPassword: string) {
+    return (formGroup: FormGroup) => {
+      const passwordControl = formGroup.controls[password];
+      const confirmPasswordControl = formGroup.controls[confirmPassword];
+
+      if (!passwordControl || !confirmPasswordControl) {
+        return null;
+      }
+
+      if (passwordControl.value !== confirmPasswordControl.value) {
+        confirmPasswordControl.setErrors({ passwordMismatch: true });
+      } else {
+        confirmPasswordControl.setErrors(null);
+      }
+    };
+  }
+}
